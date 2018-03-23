6caf728478dc7cc19960ca81575b80ca7f9e1b05
Zeyu Wang <wzy.franklin@gmail.com>
2018-03-23 16:01:11 +1100

    basic code for static server without style

---
+++ src/helper/route.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const handlebars = require('handlebars');
+const promisify = require('util').promisify;
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
+const config = require('../config/defaultConfig');
+
+const templatePath = path.join(__dirname, '../template/dir.html');
+const source = fs.readFileSync(templatePath);
+const template = handlebars.compile(source.toString());
+
+module.exports = async function(req, res, filePath) {
+    try {
+        const stats = await stat(filePath);
+        if (stats.isFile()) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            fs.createReadStream(filePath).pipe(res);
+        } else if (stats.isDirectory()) {
+            const files = await readdir(filePath);
+            const dir = path.relative(config.root, filePath);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '',
+                files,
+
+            };
+            res.end(template(data));
+        }
+    } catch (error) {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`${filePath} is not a file or directory\n ${error}`);
+    }
+}
